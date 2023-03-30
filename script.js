9fa848b9be4b1e8037135def668a83f263007e14
zzanho <leechanho838@gmail.com>
2023-03-29 22:53:34 -0700

    made hovering effect

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 const gridContainer = document.querySelector('#grid-container');
-const squares = document.querySelectorAll(".square");
 
 for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++){
-        const square = document.createElement('div');
-     square.classList.add('square');
-     gridContainer.appendChild(square);
-    }
-}
+  for (let j = 0; j < 16; j++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    gridContainer.appendChild(square);
 
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-      square.classList.add("hovered");
+    square.addEventListener('mouseover', () => {
+      square.classList.add('hovered');
     });
-    square.addEventListener("mouseout", () => {
-      square.classList.remove("hovered");
+
+    square.addEventListener('mouseout', () => {
+      square.classList.remove('hovered');
     });
-  });
+  }
+}
