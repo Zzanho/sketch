1a63e4e1c12aedb6ec2d3958987d54e799844f0b
zzanho <leechanho838@gmail.com>
2023-03-29 23:00:56 -0700

    adjusting grid size with pop up

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const gridContainer = document.querySelector('#grid-container');
+const newGridButton = document.querySelector('#new-grid-button');
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    gridContainer.appendChild(square);
-
-    square.addEventListener('mouseover', () => {
-      square.classList.add('hovered');
-    });
-
-    square.addEventListener('mouseout', () => {
-      square.classList.remove('hovered');
-    });
+function createGrid(numSquaresPerSide) {
+  // Remove existing grid
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
   }
+
+  // Create new grid
+  for (let i = 0; i < numSquaresPerSide; i++) {
+    for (let j = 0; j < numSquaresPerSide; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      gridContainer.appendChild(square);
+
+      square.addEventListener('mouseover', () => {
+        square.classList.add('hovered');
+      });
+    }
+  }
+
+  // Adjust square size based on new number of squares per side
+  const squareSize = `calc(960px / ${numSquaresPerSide})`;
+  const squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    square.style.width = squareSize;
+    square.style.height = squareSize;
+  });
 }
+
+newGridButton.addEventListener('click', () => {
+  let numSquaresPerSide = prompt('Enter the number of squares per side (max 100):');
+  numSquaresPerSide = Math.min(numSquaresPerSide, 100); // Limit to max of 100 squares
+  createGrid(numSquaresPerSide);
+});
+
+// Create initial grid
+createGrid(16);
